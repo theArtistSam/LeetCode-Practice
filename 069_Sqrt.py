20cce4e4790325f5feb2c8620ccf450ce3dc66cd
unknown <theartistsam47@gmail.com>
2024-09-01 19:18:47 +0500

    069 - Sqrt(not completed)

---
+++ 069_Sqrt.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        # Still needs to be completed
+        pass
+
+
+# Test the solution
+s = Solution()
+print(s.mySqrt(4))
+# s = (4)^1/2
+#
+# 8
+# 2 * 2 * 2 = 8
+# (8)1/2
+# (2*(2)2)1/2
