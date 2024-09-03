81617f02b4e13408417c45a74933dbb949c367f9
unknown <theartistsam47@gmail.com>
2024-09-03 23:52:02 +0500

    070 - ClimbingStairs(Pending)

--- 070_ClimbingStairs.py
+++ 070_ClimbingStairs.py
@@ -1,6 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # works but let's reduce the time complexity
+        # Still needed to be worked on!
         if n == 0 or n == 1:
             return 1
         else:
